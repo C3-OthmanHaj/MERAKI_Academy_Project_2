523f44779ee47a82ae5816ec6ad908943ad1c77c
Othman Abu  Al-Haj <othman.abualhaj20@gmail.com>
2021-08-09 13:40:49 +0300

    3

---
+++ main.js
@@ -0,0 +1,104 @@
+const body = $("body")
+body.append(body)
+
+books.append(books)
+
+const books = [
+    {
+        name: "The Full Stack Developer",
+        Year: 2018,
+        author: "Chris Northwood",
+        description:  "Understand the technical foundations, as well as the non-programming skills needed to be a successful full stack web developer. This book reveals the reasons why a truly successful full stack developer does more than write code. You will learn the principles of the topics needed to help a developer new to agile or full stack working―UX, project management, QA, product management, and more― all from the point of view of a developer.",
+    },
+    {
+        name :	"JavaScript: The Good Parts" ,
+        author : "Douglas Crockford" ,
+        year :  2008 ,
+        description : "Most programming languages contain good and bad parts, but JavaScript has more than its share of the bad, having been developed and released in a hurry before it could be refined. This authoritative book scrapes away these bad features to reveal a subset of JavaScript that’s more reliable, readable, and maintainable than the language as a whole-a subset you can use to create truly extensible and efficient code."
+    },
+    {
+        name : "The crisis" ,
+        author : "Winston Churchill" ,
+        year : 1901 ,
+        description : "The Crisis is an historical novel published in 1901 by the American novelist Winston Churchill.[1] It was the best-selling book in the United States in 1901. The novel is set in the years leading up to the first battles of the American Civil War, mostly in the divided state of Missouri. It follows the fortunes of young Stephen Brice, a man with Union and abolitionist sympathies, and his involvement with a Southern family." ,
+    },
+    {
+        name : "The Fault in Our Stars" ,
+        author :"John Green" ,
+        year : 2012  ,
+        description :"The Fault in Our Stars is a novel by John Green. It is his fourth solo novel, and sixth novel overall. It was published on January 10, 2012. The title is inspired by Act 1, Scene 2 of Shakespeare's play Julius Caesar, in which the nobleman Cassius says to Brutus: The fault, dear Brutus, is not in our stars, But in ourselves, that we are underlings. The story is narrated by Hazel Grace Lancaster, a 16-year-old girl with thyroid cancer that has affected her lungs. Hazel is forced by her parents to attend a support group where she subsequently meets and falls in love with 17-year-old Augustus Waters, an ex-basketball player and amputee." ,
+    },
+    {
+        name : "The Da Vinci Code" ,
+        author :"Dan Brown" ,
+        year : 2003 ,
+        description :"While in Paris on business, Harvard symbologist Robert Langdon receives an urgent late-night phone call: the elderly curator of the Louvre has been murdered inside the museum. Near the body, police have found a baffling cipher. Solving the enigmatic riddle, Langdon is stunned to discover it leads to a trail of clues hidden in the works of da Vinci…clues visible for all to see…and yet ingeniously disguised by the painter." ,
+    },
+    {
+        name : "Stephen King's Danse Macabre" ,
+        author :"Stephen King" ,
+        year :  1985,
+        description :"From the Flap In his most unusual bestseller, the man who knows better than anyone else in the world what scares us and why, takes us on his personal--and terrifying--tour of the dark ballroom of Horror itself." ,
+    },
+    {
+        name : ""
+        author :""
+        year :
+        description :""
+    },
+    {
+        name : ""
+        author :""
+        year :
+        description :""
+    },
+    {
+        name : ""
+        author :""
+        year :
+        description :""
+    },
+    {
+        name : ""
+        author :""
+        year :
+        description :""
+    },
+    {
+        name : ""
+        author :""
+        year :
+        description :""
+    },
+    {
+        name : ""
+        author :""
+        year :
+        description :""
+    },
+    {
+        name : ""
+        author :""
+        year :
+        description :""
+    },
+    {
+        name : ""
+        author :""
+        year :
+        description :""
+    },
+    {
+        name : ""
+        author :""
+        year :
+        description :""
+    },
+    {
+        name : ""
+        author :""
+        year :
+        description :""
+    }
+
+];
